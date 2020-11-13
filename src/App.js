cdd4e5008e941a1fd1932a8a1131c48c8ee97a8c
Anton Pchelkin <imanbee@gmail.com>
2020-11-13 10:43:53 +0300

    Display result in right column

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
+import React, {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
+import {Controlled as CodeMirror} from 'react-codemirror2'
+import 'codemirror/mode/yaml/yaml';
+import axios from 'axios';
+import Spinner from "react-svg-spinner";
 
 function App() {
+  const [crd, setCRD] = useState("");
+  const [cr, setCR] = useState("");
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const onCRDChange = async (editor, data, value) => {
+    if (value !== '') {
+      setLoading(true);
+      setError(false);
+      try {
+        const response = await axios.post('https://us-central1-crd2cr.cloudfunctions.net/crd2cr', value)
+        if (response) {
+          
+          setCR(response.data);  
+          setLoading(false);
+        }
+        
+      } catch (error) {
+        setLoading(false);
+        setError(true);
+      }
+      
+    }
+  }
+
+  const onCRChange = (editor, data, value) => {
+    console.log(value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+      <CodeMirror
+        value={crd}
+        options={{
+          mode: 'yaml',
+          theme: 'material',
+          lineNumbers: true,
+          lineWrapping: true
+        }}
+        onBeforeChange={(editor, data, value) => {
+          setCRD(value)
+        }}
+        onChange={onCRDChange}
+      />
+      <CodeMirror
+        value={cr}
+        options={{
+          mode: 'yaml',
+          theme: 'material',
+          lineNumbers: true,
+          lineWrapping: true
+        }}
+        onBeforeChange={(editor, data, value) => {
+          setCR(value)
+        }}
+        onChange={onCRChange}
+      />
+      {error && <div class="error">Can't convert this value</div>}
+      {loading && <div class="loading"><Spinner size={90} /></div>}
     </div>
   );
 }
